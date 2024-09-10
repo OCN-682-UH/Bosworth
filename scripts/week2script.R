9e242aa9b7ba8764ee0edabaef8389e4a8aee2b6
Kylebosworth <kylem6@hawaii.edu>
2024-09-10 09:51:59 -1000

    add to Git

---
+++ scripts/week2script.R
@@ -0,0 +1,14 @@
+### This is my first script. i am learning how to import data
+### Created by: Kyle Bosworth
+### Created on: 2024-09-09
+##################################################################
+### Load libraries ########## 
+library(tidyverse)
+library(here)
+## here() starts at C:/Users/boz/Desktop/Repositories/Bosworth
+### Read in data ###
+weightdata <-read_csv("data/weightdata.csv")
+### Data Analysis ####
+head(weightdata) # Looks at the top 6 lines of the data frame
+tail(weightdata) # Looks at the bottom 6 lines of the data frame
+view(weightdata) # Opens a new window to look at the entire data frame
