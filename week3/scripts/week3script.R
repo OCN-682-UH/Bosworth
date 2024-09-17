11c04585604f81f1c099958a57e92649230cd9db
Kylebosworth <kylem6@hawaii.edu>
2024-09-16 21:24:08 -1000

    updatenew

--- week3/scripts/week3script.R
+++ week3/scripts/week3script.R
@@ -16,21 +16,24 @@ glimpse(penguins)
 # make a plot
 ggplot(data = penguins,
        mapping = aes(x = bill_depth_mm, ## "," are used to add on layers
-                     y = bill_depth_mm,
-                     geom_point(size = 1, alpha = 0.25), # manipulating the size of point
+                     y = bill_length_mm,
                      color = island,
-                     shape = island, # shape is discrete
-                     size = body_mass_g)) +  # size is continuous ## "+" is used to connect functions 
-  geom_point() + 
- # X as a function of Y = (x~y)
-  # facet_grid(island ~ species) +  #row as a function of columns, a 2D grid 
-  facet_wrap(island ~ species, ncol = 2) + #ncol = make it "n" columns
-  guides(color = FALSE) + # you have more control using facet_wrap
-  labs(title = "Bill length x Bill depth per island",
-       subtitle = "Woah!",
+                     shape = island)) + # shape is discrete # size is continuous ## "+" is used to connect functions 
+  geom_point(size = 1, alpha = 0.5) +  # manipulating the size of point
+  geom_violin() + 
+  facet_wrap(island ~ species, ncol = 2) +  
+# X as a function of Y = (x~y)
+# facet_grid(x ~ y) +  #row as a function of columns, a 2D grid 
+# ncol = make it "n" columns 
+# you have more control using facet_wrap
+  labs(title = "Bill Length vs Bill Depth by Island and Species",
+       subtitle = "Palmer Penguins Dataset",
        x = "Bill Depth (mm)",
        y = "Bill Length (mm)",
-       color = "penguin tuxedo style",
-       shape = "penguin tuxedo style",
-       caption = "source: Palmer Penguins package/palmer LTER") +
-  scale_color_viridis_d()
+       color = "Island",
+       shape = "Island",
+       caption = "Source: Palmer Penguins package/Palmer LTER") +
+  scale_color_viridis_d() +
+  theme(legend.position = "bottom",  # legend. position to move legend to bottom
+        plot.title = element_text(hjust = 0.5),  # plot title center
+        plot.subtitle = element_text(hjust = 0.5))  # plot subtitle center
